3cb0c6271d6da5bd691b89bd89a58510aaeadffd
Dhara Bagadiya <dharabagadiya1990@gmail.com>
2020-02-16 09:51:07 -0800

    Made Task for Test

--- AzureFunctionApp.FunctionBusiness.Tests/BusinessHttpTriggerTest.cs
+++ AzureFunctionApp.FunctionBusiness.Tests/BusinessHttpTriggerTest.cs
@@ -24,6 +24,7 @@ namespace AzureFunctionApp.FunctionBusiness.Tests
         public Mock<ExecutionContext> MockExecutionContext;
         public Processor<BusinessHttpTriggerAsync> HttpProcessor;
         Mock<HttpRequest> MockHttpRequest;
+        Mock<HttpRequest> MockHttpRequest_NoValue;
         #endregion
 
         #region Private Methods
@@ -34,12 +35,21 @@ namespace AzureFunctionApp.FunctionBusiness.Tests
             var body = "";
             query.TryAdd("name", "Mehul");
             MockHttpRequest.Setup(req => req.Query).Returns(new QueryCollection(query));
-             var stream = new MemoryStream();
+            var stream = new MemoryStream();
             var writer = new StreamWriter(stream);
             writer.Write(body);
             writer.Flush();
             stream.Position = 0;
             MockHttpRequest.Setup(req => req.Body).Returns(stream);
+
+            MockHttpRequest_NoValue = new Mock<HttpRequest>();
+            MockHttpRequest_NoValue.Setup(req => req.Query).Returns(new QueryCollection(new Dictionary<String, StringValues>()));
+            stream = new MemoryStream();
+            writer = new StreamWriter(stream);
+            writer.Write("");
+            writer.Flush();
+            stream.Position = 0;
+            MockHttpRequest_NoValue.Setup(req => req.Body).Returns(stream);
 
             MockLogger = new Mock<ILogger>();
             MockExecutionContext = new Mock<ExecutionContext>();
@@ -53,10 +63,35 @@ namespace AzureFunctionApp.FunctionBusiness.Tests
         }
 
         [Fact]
-        public void Test1()
+        public void Function_Name_Value()
         {
             HttpProcessor.Run(MockHttpRequest.Object);
             Assert.Equal(new OkObjectResult("Hello, Mehul").Value, ((OkObjectResult)HttpProcessor.Action).Value);
+        }
+
+        [Fact]
+        public void Function_Name_Value_Body()
+        {
+            MockHttpRequest = new Mock<HttpRequest>();
+            var query = new Dictionary<String, StringValues>();
+            var body = @"{ 'name' : 'Mehul' }";
+            MockHttpRequest.Setup(req => req.Query).Returns(new QueryCollection(query));
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(body);
+            writer.Flush();
+            stream.Position = 0;
+            MockHttpRequest.Setup(req => req.Body).Returns(stream);
+
+            HttpProcessor.Run(MockHttpRequest.Object);
+            Assert.Equal(new OkObjectResult("Hello, Mehul").Value, ((OkObjectResult)HttpProcessor.Action).Value);
+        }
+
+        [Fact]
+        public void Function_Name_NoValue()
+        {
+            HttpProcessor.Run(MockHttpRequest_NoValue.Object);
+            Assert.Equal(new BadRequestObjectResult("Please pass a name on the query string or in the request body").Value, ((Microsoft.AspNetCore.Mvc.BadRequestObjectResult)HttpProcessor.Action).Value);
         }
     }
 }
